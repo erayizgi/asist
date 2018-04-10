97b6c3f03310ede0fde8aaea1b5ab4ee15cfc6f9
erayizgi <erayizgi@gmail.com>
2018-04-10 11:34:49 +0300

    eray

---
+++ app/Activities.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activities extends Model
+{
+    protected $table = "islemler";
+    protected $primaryKey = "rec_id";
+    protected $fillable = ["kullanici_id", "islem_turu", "islem_id", "islem_tarihi"];
+}
---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Activities;
+use App\Libraries\Res;
+use App\Libraries\TReq;
+use Illuminate\Http\Request;
+
+class ActivityController extends Controller
+{
+    public function getActivities(Request $request)
+    {
+        try {
+            $query = TReq::multiple($request, Activities::class);
+            $data = $query["query"]
+                ->select("islemler.islem_turu", "islemler.islem_id", "islemler.islem_tarihi",'tb_kullanicilar.adSoyad', 'tb_kullanicilar.IMG', 'tb_kullanicilar.kullaniciAdi')
+                ->join("tb_takip", "tb_takip.takipEdilenID", "islemler.kullanici_id", "inner")
+                ->join("tb_kullanicilar", "tb_kullanicilar.ID", "islemler.kullanici_id", "inner")
+                ->where("tb_takip.takipEdenID", $request->user()->ID)->get();
+            return Res::success(200, 'Activity', $data);
+        } catch (\Exception $e) {
+            $error = new \stdClass();
+            $error->errors = [
+                'exception' => [
+                    $e->getMessage()
+                ]
+            ];
+            $message = 'An error has occured!';
+            return Res::fail($e->getCode(), $message, $error);
+        }
+    }
+}
