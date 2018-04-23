1ca753d67c88ba36db74d976321bdb1d0a501746
erayizgi <erayizgi@gmail.com>
2018-04-24 00:01:14 +0300

    Eray Nightly Fixes #6

--- app/Conversation.php
+++ app/Conversation.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
 class Conversation extends Model
 {
     use SoftDeletes;
@@ -11,4 +13,13 @@ class Conversation extends Model
     protected $primaryKey = "conversation_id";
     protected $dates = ['deleted_at'];
     protected $fillable = ['sender_id', 'receiver_id'];
+    protected $appends = ["sender"];
+
+    public function getSenderAttribute()
+    {
+        $roles = DB::table('tb_kullanicilar')
+            ->where('ID',$this->attributes['sender_id'])
+            ->select('tb_kullanicilar.IMG','tb_kullanicilar.adSoyad','tb_kullanicilar.IMG','tb_kullanicilar.kullaniciAdi')->first();
+        return  $roles;
+    }
 }
--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Conversation;
 use App\Message;
+use App\Notifications;
 use Exception;
 use App\Libraries\TReq;
 use App\Libraries\Res;
@@ -15,158 +16,161 @@ use Illuminate\Validation\ValidationException;
 
 class ConversationController extends Controller
 {
-    public function inbox(Request $request){
-        try{
+    public function inbox(Request $request)
+    {
+        try {
             $query = TReq::multiple($request, Conversation::class);
             $data = $query['query']->where('receiver_id', $request->user()->ID)->get();
             $result = [
-                'metadata'=>[
-                    'count'=>$data->count(),
-                    'offset'=>$query['offset'],
-                    'limit'=>$query['limit'],
+                'metadata' => [
+                    'count' => $data->count(),
+                    'offset' => $query['offset'],
+                    'limit' => $query['limit'],
                 ],
             ];
 
-            foreach($data as $conversation)
-            {
-                $result['conversations'] = $conversation;
-                $result['conversations']['message'] = Message::where('conversation_id', $conversation['conversation_id'])->get();
+            foreach ($data as $conversation) {
+                $result['conversations'][] = [
+                    "conversation" => $conversation,
+                    "message" => Message::where('conversation_id', $conversation['conversation_id'])->get()
+                ];
+
             }
 
-            return Res::success(200,'inbox',$result);
+            return Res::success(200, 'inbox', $result);
         } catch (Exception $e) {
             return Res::fail($e->getCode(), $e->getMessage());
         }
     }
 
-    public function outbox(Request $request){
-        try{
+    public function outbox(Request $request)
+    {
+        try {
             $query = TReq::multiple($request, Conversation::class);
             $data = $query['query']->where('sender_id', $request->user()->ID)->get();
             $result = [
-                'metadata'=>[
-                    'count'=>$data->count(),
-                    'offset'=>$query['offset'],
-                    'limit'=>$query['limit'],
+                'metadata' => [
+                    'count' => $data->count(),
+                    'offset' => $query['offset'],
+                    'limit' => $query['limit'],
                 ],
             ];
 
 
-            foreach($data as $conversation)
-            {
+            foreach ($data as $conversation) {
                 $result['conversations'] = $conversation;
                 $result['conversations']['message'] = Message::where('conversation_id', $conversation['conversation_id'])->get();
             }
 
-            return Res::success(200,'outbox',$result);
+            return Res::success(200, 'outbox', $result);
         } catch (Exception $e) {
             return Res::fail($e->getCode(), $e->getMessage());
         }
     }
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         // If conversation has been created successfully create a new message
-        try{
+        try {
             $validator = Validator::make($request->all(), [
                 'receiver_id' => 'required|filled',
-                'message'     => 'required|filled|min:1'
+                'message' => 'required|filled|min:1'
             ]);
 
-            if($validator->fails()){
-                throw new ValidationException($validator,Response::HTTP_BAD_REQUEST,$validator->errors());
+            if ($validator->fails()) {
+                throw new ValidationException($validator, Response::HTTP_BAD_REQUEST, $validator->errors());
             }
 
             $conversation_id = Conversation::create([
-                'sender_id'    => $request->user()->ID,
-                'receiver_id'  => $request->receiver_id,
+                'sender_id' => $request->user()->ID,
+                'receiver_id' => $request->receiver_id,
             ]);
 
 
-            if(!Message::create(['conversation_id' => $conversation_id->conversation_id, 'user_id' => $request->user()->ID, 'content' => $request->message])) {
+            if (!Message::create(['conversation_id' => $conversation_id->conversation_id, 'user_id' => $request->user()->ID, 'content' => $request->message])) {
                 throw new Exception("conversation error", Response::HTTP_INTERNAL_SERVER_ERROR);
             }
 
             $notification = [
-                "alici_id"      => $request->receiver_id,
+                "alici_id" => $request->receiver_id,
                 "bildirim_tipi" => 'pm',
-                "bildirim_url"  => "pm_url",
-                "olusturan_id"  => $request->user()->ID
+                "bildirim_url" => "pm_url",
+                "olusturan_id" => $request->user()->ID
             ];
 
-            if(!Notifications::insert($notification)){
+            if (!Notifications::insert($notification)) {
                 throw new Exception('notification errors', Response::HTTP_INTERNAL_SERVER_ERROR);
             }
 
-            return Res::success(200,'pm', 'success');
+            return Res::success(200, 'pm', 'success');
 
-        } catch (ValidationException $e){
-            return Res::fail($e->getResponse(),$e->getMessage(),$e->errors());
+        } catch (ValidationException $e) {
+            return Res::fail($e->getResponse(), $e->getMessage(), $e->errors());
         } catch (Exception $e) {
             return Res::fail($e->getCode(), $e->getMessage());
         }
     }
 
-    public function reply(Request $request){
-        try{
+    public function reply(Request $request)
+    {
+        try {
             $validator = Validator::make($request->all(), [
-                'message'         => 'required|filled|min:1',
+                'message' => 'required|filled|min:1',
                 'conversation_id' => 'required|filled',
             ]);
 
-            if($validator->fails()){
-                throw new ValidationException($validator,Response::HTTP_BAD_REQUEST,$validator->errors());
+            if ($validator->fails()) {
+                throw new ValidationException($validator, Response::HTTP_BAD_REQUEST, $validator->errors());
             }
-
-            if(Message::create(['conversation_id' => $request->conversation_id, 'user_id' => $request->user()->ID, 'content' => $request->message])){
-                return Res::success(200,'pm', 'success');
-            }else {
-                throw new Exception('an error', Response::HTTP_INTERNAL_SERVER_ERROR);
-            }
-        } catch (ValidationException $e){
-            return Res::fail($e->getResponse(),$e->getMessage(),$e->errors());
+            Message::create(['conversation_id' => $request->conversation_id, 'user_id' => $request->user()->ID, 'content' => $request->message]);
+            return Res::success(200, 'pm', 'success');
+        } catch (ValidationException $e) {
+            return Res::fail($e->getResponse(), $e->getMessage(), $e->errors());
         } catch (Exception $e) {
             return Res::fail($e->getCode(), $e->getMessage());
         }
     }
 
-    public function read(Request $request, $id){
-        try{
+    public function read(Request $request, $id)
+    {
+        try {
             $query = TReq::multiple($request, Conversation::class);
             $data = $query['query']->where('conversation_id', $id)->first();
             $result = [
-                'metadata'=>[
-                    'count'=>$data->count(),
-                    'offset'=>$query['offset'],
-                    'limit'=>$query['limit'],
+                'metadata' => [
+                    'count' => $data->count(),
+                    'offset' => $query['offset'],
+                    'limit' => $query['limit'],
                 ],
-                'data'=>$data
+                'data' => $data
             ];
 
-            return Res::success(200,'Conversations',$result);
+            return Res::success(200, 'Conversations', $result);
         } catch (Exception $e) {
             return Res::fail($e->getCode(), $e->getMessage());
         }
     }
 
-    public function delete(Request $request){
-        try{
+    public function delete(Request $request)
+    {
+        try {
             $validator = Validator::make($request->all(), [
                 'conversation_id' => 'required'
             ]);
 
-            if($validator->fails()){
-                throw new ValidationException($validator,Response::HTTP_BAD_REQUEST,$validator->errors());
+            if ($validator->fails()) {
+                throw new ValidationException($validator, Response::HTTP_BAD_REQUEST, $validator->errors());
             }
 
 
-            if(!Conversation::find($request->conversation_id)->delete()){
+            if (!Conversation::find($request->conversation_id)->delete()) {
                 throw new Exception('an error', Response::HTTP_INTERNAL_SERVER_ERROR);
             }
 
-            return Res::success(200,'success', 'success');
+            return Res::success(200, 'success', 'success');
 
-        } catch (ValidationException $e){
-            return Res::fail($e->getResponse(),$e->getMessage(),$e->errors());
+        } catch (ValidationException $e) {
+            return Res::fail($e->getResponse(), $e->getMessage(), $e->errors());
         } catch (Exception $e) {
             return Res::fail($e->getCode(), $e->getMessage());
         }
