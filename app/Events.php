18e5eb902e1f5e9332f116623cfe7d17e7dd85fb
cebozkurt <ceyhunemrebozkurt@gmail.com>
2018-04-28 01:42:14 +0300

    Points and Coupons C2

--- app/Events.php
+++ app/Events.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class Events extends Model
 {
     protected $primaryKey = "event_id";
@@ -21,4 +21,24 @@ class Events extends Model
         "away",
         "identifier_id"
     ];
+
+    protected $appends = ['odds'];
+
+    public function getOdssAtribute(){
+
+
+        //SELECT odd_options.odd_type_id FROM odd_options
+        //INNER JOIN odd_types ON odd_types.odd_type_id = odd_options.odd_type_id
+        //WHERE odd_options.event_id = 14449
+        //GROUP BY odd_options.odd_type_id
+        //
+
+        return DB::table('odd_options')
+            ->select('odd_options.odd_type_id')
+            ->join('odd_types', 'odd_types.odd_type_id', 'odd_options_odd_type_id')
+            ->where('odd_options.event_id', $this->attributes['event_id'])
+            ->groupBy('odd_options.odd_type_id')->count();
+
+        //$this->attributes['event_id'];
+    }
 }
